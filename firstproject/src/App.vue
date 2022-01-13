5c3f9164f14107930bbdd958ef6a8d93505afbc7
berkaynayman <berkaynayman4@gmail.com>
2022-01-13 23:56:54 +0300

    ClassBindings-InputValidation

--- firstproject/src/App.vue
+++ firstproject/src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <div>
+    <!--
     <HelloWorld userName="Berkay Nayman"/>
     <CounterApp />
+    -->
+    <ClassBindings />
   </div>
 </template>
 
 <script>
+/*
 import HelloWorld from './components/props-degiskenTanımlama.vue'
 import CounterApp from './components/counterApp-ifElse-for-computedProperties.vue'
+*/
+import ClassBindings from './components/ClassBindings-InputValidation.vue'
 
 export default {
   name: 'App',
   components: {
+    /*
     HelloWorld,
-    CounterApp
+    CounterApp,
+    */
+    ClassBindings
   }
 }
 </script>
---
+++ firstproject/src/components/ClassBindings-InputValidation.vue
@@ -0,0 +1,48 @@
+<template>
+    <div> 
+        <div v-for="number in numbers" :key="number" v-bind:class="getClass(number)">
+            {{ number }}
+        </div>
+        <div>
+            <input v-bind:value="value" 
+                   v-on:input="input" />
+            {{ value }}
+            <h4>{{ error }}</h4>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ClassBindings',
+        data(){
+            return{
+                numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                value: "user"
+            }
+        },
+        computed:{
+            error(){
+                if (this.value.length < 5 ) {
+                    return "Kelime uzunluğu 5 den küçük."
+                }
+            }
+        },
+        methods:{
+            input($event){
+                this.value = $event.target.value
+            },
+            isEven(num) {
+                return num % 2 === 0
+            },
+            getClass(num) {
+                return this.isEven(num) ? "blue" : "red"
+            }
+        }
+    }
+</script>
+
+<style>
+    .blue{color:blue}
+    .red{color:red}
+</style>
