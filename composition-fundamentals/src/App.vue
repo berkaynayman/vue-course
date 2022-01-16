8488e1367c4f6c4f37307a7e7d9d03dd5600d297
berkaynayman <berkaynayman4@gmail.com>
2022-01-16 13:36:17 +0300

    ref, reactive, computed, watch, watchEffect

---
+++ composition-fundamentals/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <button @click="a++">{{ a }}</button>
+    <button @click="b++">{{ b }}</button>
+    <h3>{{ total }}</h3>
+    <p>
+      {{ history }}
+    </p>
+  </div>
+</template>
+
+<script>
+import { useNumbers } from "./useNumbers.js" 
+
+export default {
+  name: 'App',
+  setup(){
+
+    /*
+    const numbers = reactive({
+      a: 0,
+      b: 0
+    })
+    const increase = (n) => {
+      numbers[n]++
+    } 
+
+    const total = computed(() => {
+      return count.value + numbers.a + numbers.b
+    }) 
+
+    watch(numbers, (newVal) => {
+      console.log(`a: ${newVal.a}. b: ${newVal.b}`);
+    })
+
+    watchEffect(() => {
+      console.log(numbers.a);
+      console.log(numbers.b);
+    })
+    */
+
+    return useNumbers()
+  }
+}
+</script>
+
+<style>
+
+</style>
